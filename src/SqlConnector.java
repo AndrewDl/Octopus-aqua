1e1b210d74de5bc0cab23d80b432895042d20e8d
AndrewDl <dlandrew@ukr.net>
2016-03-15 21:56:57 +0200

    Octopus-Aqua v0.01a pre (build 4) ----------------------------------------------------------------------------- Classes: ----------------------------------------------------------------------------- SqlConnector      private void connectToSql()        throws ClassNotFoundException, SQLException
           -connects to DB using default settings:
               private String dbname = "octopus-aqua";
               private String port = "3306";
               private String hostname = "localhost";
    
               private String url = "jdbc:mysql://localhost:3306/octopus-aqua";
    
               private String username = "mysql";
               private String password = "mysql";
    
         private void connectToSql(String username, String password)
           throws ClassNotFoundException, SQLException
    
           -connects to DB using username and password;

--- src/SqlConnector.java
+++ src/SqlConnector.java
@@ -1,5 +1,27 @@
+import java.sql.*;
 /**
  * Created by Andrew on 02.03.2016.
  */
 public class SqlConnector {
+
+    private String dbname = "octopus-aqua";
+    private String port = "3306";
+    private String hostname = "localhost";
+
+    private String url = "jdbc:mysql://localhost:3306/octopus-aqua";
+
+    private String username = "mysql";
+    private String password = "mysql";
+
+    private void connectToSql() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(url, username, password);
+        conn.close();
+    }
+
+    private void connectToSql(String username, String password) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://" + hostname + ":" + port + "/" + dbname, username, password);
+        conn.close();
+    }
 }
